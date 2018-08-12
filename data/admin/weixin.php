62c1068e8a319f6d8f0d37e0dd28390c34c6321f
ning15 <ning15@126.com>
2018-08-12 20:05:23 +0800

    初始化电影网代码

---
+++ data/admin/weixin.php
@@ -0,0 +1,1 @@
+<?php define("isopen", "n"); define("title", "好电影网"); define("url", "http://movie.dev.com"); define("ckmov_url", "http://www.seacms.net/ck/"); define("follow", "感谢您的关注。"); define("noc", "暂无你要的内容。"); define("dpic", "http://www.seacms.net/api/wx.jpg"); define("help", "这是帮助信息。"); define("topage", "d"); define("sql_num", "5");  ?>
---
+++ data/common.inc.php
@@ -0,0 +1,9 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'seacms';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = 'root';
+$cfg_dbprefix = 'sea_';
+$cfg_db_language = 'utf8';
+?>
